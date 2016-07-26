21928802ec73759e0125af80c72e9bfa1175182e
vredd21 <vredd21@LH7U05CG5281YP2.ms.ds.uhc.com>
2016-07-26 18:23:43 -0500

    Deftect Fix # 7878

--- CoreJavaProject/src/com/test/examples/ArgumentsDemoApp.java
+++ CoreJavaProject/src/com/test/examples/ArgumentsDemoApp.java
@@ -3,19 +3,19 @@ package com.test.examples;
 public class ArgumentsDemoApp {
 
 	public static void main(String[] args) {
-		System.out.println(" Total Params :" + args.length);
+		System.out.println(" Total Params : === " + args.length);
 		System.out.println(" args[0] " + args[0]);
 		System.out.println(" args[1] " + args[1]);
 		System.out.println(" args[2] " + args[2]);
 		
-		System.out.println("Sno Name " + System.getProperty("sno.name"));
+		System.out.println("Sno Name :" + System.getProperty("sno.name"));
 		demoParam(args);
 	}
 	
-	
+	//Demo Method
 	public static void demoParam(String[] args){
 		//System.out.println(" args[2] " + args[2]);
-		System.out.println("Client Name " + System.getProperty("clientName"));
+		System.out.println("Client Name  :: " + System.getProperty("clientName"));
 	}
 	
 }
